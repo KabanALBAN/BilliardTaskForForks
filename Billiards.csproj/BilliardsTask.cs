9f28ec614db68e19a5191525e67d575506351b8f
KabanALBAN <56335353+KabanALBAN@users.noreply.github.com>
2019-11-11 09:38:30 +0400

    Add files via upload

--- Billiards.csproj/BilliardsTask.cs
+++ Billiards.csproj/BilliardsTask.cs
@@ -12,8 +12,20 @@ namespace Billiards
         /// <returns></returns>
         public static double BounceWall(double directionRadians, double wallInclinationRadians)
         {
-            //TODO
-            return 0.0;
+            double direction = (directionRadians * 180) / Math.PI;
+            double wallInclination = (wallInclinationRadians * 180) / Math.RI;
+
+            double wallAgle = 90 + wallInclination;
+            double ballAgle = 180 + direction;
+
+            angle %= 360;
+            if (angle > 180)
+            {
+                angle -= 360;
+            }
+
+            return (angle * Math.PI) / 180;
+
         }
     }
 }
